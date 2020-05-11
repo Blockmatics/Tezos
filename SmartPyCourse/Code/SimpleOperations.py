901c73dbe1fe4d19cb5d60b4dd60320621f524bf
Sol Lederer <sol.lederer@gmail.com>
2020-05-11 11:54:23 -0400

    Update to use scenarios

--- SmartPyCourse/Code/SimpleOperations.py
+++ SmartPyCourse/Code/SimpleOperations.py
@@ -1,48 +1,44 @@
 import smartpy as sp
+
 class SimpleOperations(sp.Contract):
     def __init__(self):
         self.init(storedValue = 4, flag = True, deck = [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         
-    @sp.entryPoint
+    @sp.entry_point
     def set(self, params):
         self.data.storedValue = params.op
      
-    @sp.entryPoint       
+    @sp.entry_point       
     def double(self, params):
         self.data.storedValue *= 2
         
-    @sp.entryPoint       
+    @sp.entry_point       
     def divide(self, params):
         sp.verify(params.op > 0)
         self.data.storedValue /= params.op
         
-    @sp.entryPoint       
+    @sp.entry_point       
     def factorial(self, params):
         self.data.storedValue = 1
         sp.for x in sp.range(1, params.op+1):
             self.data.storedValue *= x
             
-    @sp.entryPoint       
+    @sp.entry_point       
     def reverseFlag(self, params):
         self.data.flag = ~self.data.flag
         
 # Defines the contract test.
-@addTest(name = "Test simple operations")
+@sp.add_test(name = "Test simple operations")
 def testSimpleOperations():
    # Instantiates the contract.
    myContract = SimpleOperations()
-   
-   # Declares a string variable, to build a html output.
-   html = myContract.fullHtml()
 
-   # Calls the contract set method and converts output to html.
-   html += myContract.set(op = 9).html()
-   html += myContract.set(op = 17).html()
+   scenario = sp.test_scenario()
+   scenario += myContract
 
-   html += myContract.divide(op = 6).html()
-   html += myContract.factorial(op = 11).html()
-   html += myContract.reverseFlag().html()
+   scenario += myContract.set(op = 9)
+   scenario += myContract.set(op = 17)
 
-   
-   # Outputs to screen.
-   setOutput(html)
+   scenario += myContract.divide(op = 6)
+   scenario += myContract.factorial(op = 11)
+   scenario += myContract.reverseFlag()
